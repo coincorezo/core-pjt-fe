3eaa8ab2cab49df698ec8c0123336d41fc68630d
zeroh0 <umberto1900@naver.com>
2024-06-06 05:34:48 +0900

    fix: more typescript Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,16 +1,27 @@
-export default function Button({
-  children,
-  className,
-  onClick,
-  disabled = false
-}: {
+'use client'
+
+// type ButtonType = string
+type ButtonType = 'button' | 'submit' | 'reset'
+
+interface Props {
   children: React.ReactNode
-  className: string
-  onClick: () => void
+  type?: ButtonType
+  className?: string
+  onClick?: () => void
   disabled?: boolean
-}) {
+}
+
+// const Button: React.FC<Props> = ({ children, type = 'button', className = 'button', onClick, disabled }) => {
+//   return (
+//     <button type={type} className={className} onClick={onClick} disabled={disabled}>
+//       {children}
+//     </button>
+//   )
+// }
+
+export default function Button({ children, type = 'button', className = 'button', onClick, disabled }: Props) {
   return (
-    <button type="button" className={className} onClick={onClick} disabled={disabled}>
+    <button type={type} className={className} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   )
