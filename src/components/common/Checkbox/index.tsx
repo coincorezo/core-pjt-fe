a0a9b0cfc76f4f213389b0b9928e1b49fd3374c9
zeroh0 <umberto1900@naver.com>
2024-06-07 16:41:57 +0900

    feat: Create Checkbox, Radio Component

---
+++ src/components/common/Checkbox/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useState } from 'react'
+
+interface Props {
+  children: React.ReactNode
+  name: string
+  value: string
+  checked?: boolean
+  onChange: (checked: boolean) => void
+  disabled?: boolean
+  required?: boolean
+}
+
+export default function Checkbox({ children, name, value, checked, onChange, disabled, required }: Props) {
+  const [isChecked, setIsChecked] = useState(checked)
+
+  return (
+    <>
+      <input
+        type="checkbox"
+        name={name}
+        value={value}
+        checked={isChecked}
+        onChange={e => {
+          setIsChecked(e.target.checked)
+          onChange(e.target.checked)
+        }}
+        disabled={disabled}
+        required={required}
+      />
+      {children}
+    </>
+  )
+}
