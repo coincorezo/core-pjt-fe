b525ca6176aa8ad6c06884ae48c2dc3bf514e401
JangDongHo <zeroh0@coremethod.co.kr>
2024-06-17 12:49:40 +0900

    style: logo 변경

--- src/components/common/HeaderMark.tsx
+++ src/components/common/HeaderMark.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 export default function Header({  ...props }) {
   
 return        <div className="containerImg">
-<Image src="/images/crazecoin/crazeCoinmark.png" alt="" width={150} height={150} />
+<Image src="/images/crazecoin/coin.png" alt="" width={150} height={150} />
 </div>
 }
